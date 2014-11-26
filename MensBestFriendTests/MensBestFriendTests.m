da0c48e9ca1e431861b9d8e5bbb21f2e59aa8b3b
zac1st1k <zacfirst@gmail.com>
2014-11-26 19:22:00 +1100

    Initial Commit

---
+++ MensBestFriendTests/MensBestFriendTests.m
@@ -0,0 +1,34 @@
+//
+//  MensBestFriendTests.m
+//  MensBestFriendTests
+//
+//  Created by Zac on 26/11/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MensBestFriendTests : XCTestCase
+
+@end
+
+@implementation MensBestFriendTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
